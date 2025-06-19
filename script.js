eb3a9411ecc7ef174f73cc1211078f6504a741ac
Muzamil <muzamilzubair987654@gmail.com>
2025-06-19 23:29:46 +0500

    update file

--- script.js
+++ script.js
@@ -179,25 +179,12 @@ document.addEventListener("DOMContentLoaded", function(){
 //   });
 // --------------------------------------------------------- index.html ------------------------------------
 
- // downwards in about section
+  const section = document.getElementById("expandableSection");
+  const toggleBtn = document.getElementById("toggle-btn");
+  const icon = toggleBtn.querySelector("img");
 
-// const section = document.getElementsById('expandableSection');
-// const toggleBtn = document.getElementsById('toggle-btn');
-
-//   const downarrow = `<svg width="40px" height="40px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--emojione-monotone" preserveAspectRatio="xMidYMid meet"><path d="M53.212 10.785c-11.715-11.714-30.711-11.714-42.426 0c-11.715 11.717-11.715 30.711 0 42.427c11.715 11.717 30.711 11.717 42.426 0c11.717-11.716 11.717-30.71 0-42.427M31.999 47l-20-20h40l-20 20" fill="#000000"></path></svg>`;
-//   const uparrow = `<svg width="40px" height="40px" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--emojione-monotone" preserveAspectRatio="xMidYMid meet"><path d="M53.213 10.788c-11.715-11.717-30.711-11.717-42.426 0c-11.716 11.715-11.716 30.71 0 42.426c11.715 11.715 30.711 11.715 42.426 0c11.717-11.716 11.717-30.711 0-42.426zM12 38l20-20l20 20H12z" fill="#000000"></path></svg>`;
-
-
-//   toggleBtn.addEventListener('click', ()=>{
-//     const isCollapsed = section.classList.conatins('collapsed');
-//     if(isCollapsed){
-//       section.classList.remove('collapsed');
-//       section.classList.add('expanded');
-//       toggleBtn.innerHTML = uparrow;
-//     }
-//     else{
-//       section.classList.add('collapsed');
-//       section.classList.remove('expanded');
-//       toggleBtn.innerHTML = downarrow
-//     }
-//   })
+  toggleBtn.addEventListener("click", () => {
+    section.classList.toggle("collapsed");
+    const isCollapsed = section.classList.contains("collapsed");
+    icon.src = isCollapsed ? "images/sort-down.png" : "images/sort-up.png";
+  });
